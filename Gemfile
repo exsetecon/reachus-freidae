7829cc202e1be1e48c693177122d24d5214e4e48
udbhav1995@gmail.com <ribhav2004>
2015-02-11 23:03:29 +0530

    changes 1.0

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
 gem 'rack'
-gem 'rack-recaptcha'
+gem 'rack-recaptcha', :require => 'rack/recaptcha'
 gem 'sinatra'
 gem 'pony'
 gem 'json'
--- application.rb
+++ application.rb
@@ -1,18 +1,31 @@
 before do
-  content_type :json
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['POST']
+      puts '[Params]'
+      p params
+      content_type :json
+      headers 'Access-Control-Allow-Origin' => '*',
+              'Access-Control-Allow-Methods' => ['POST']
 end
 
-set :protection, false
-set :public_dir, Proc.new { File.join(root, "_site") }
+    set :protection, false
+    set :public_dir, Proc.new { File.join(root, "_site") }
 
 post '/send_email' do
-  if recaptcha_valid?
+    url = URI.parse("https://www.google.com/recaptcha/api/siteverify")
+req = Net::HTTP::Post.new(url.request_uri)
+req.set_form_data({"secret" => "6LcLwAATAAAAACTOnf7pIuFZOsvLBe9WrqgrogOQ", "response" => params[:'g-recaptcha-response']})
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = (url.scheme == "https")
+    p url.request_uri
+    p url.host
+    p url.port
+    p url.scheme
+    p req.body.lines   
+res = http.request(req)
+  p res.body.lines
+    success, errorkey = res.body.lines.map(&:chomp)
+  if success == 'true'
     uri = URI.parse("http://formspree.io/udbhav1995@gmail.com")
-
-# Shortcut
-res = Net::HTTP.post_form(uri, {"email" => params[:inputEmail], "subject" => params[:inputSubject] , "content" => params[:content]})    
+    res = Net::HTTP.post_form(uri, {"email" => params[:inputEmail], "subject" => params[:inputSubject] , "content" => params[:content]})    
     if res
       { :message => 'success' }.to_json
     else
@@ -22,6 +35,7 @@ res = Net::HTTP.post_form(uri, {"email" => params[:inputEmail], "subject" => par
     { :message => 'failure_captcha' }.to_json
   end
 end
+
 not_found do
   { :message => 'inside not_found' }.to_json
 end
